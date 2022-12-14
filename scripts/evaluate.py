60bf79d1dde95aa4634190a47afd23b3334ce50d
Jonne Saleva <jonne@jonnesaleva.com>
2022-12-13 19:57:07 -0500

    Remove Mean F1

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,3 +1,4 @@
+import itertools as it
 import sys
 from collections import defaultdict
 from typing import Dict, List, Optional, Set, TextIO, Tuple
@@ -17,11 +18,7 @@ from tqdm import tqdm
 Computes 5 evaluation metrics for translated words:
     - Token Accuracy
     - 1 - Token Accuracy
-    - Mean F1
     - BLEU
-
-Mean F1 is computed using longest common subsequence, which
-is computed using Levenshtein distance.
 
 All scores are normalized to lie in the range [0, 1].
 """
@@ -50,13 +47,11 @@ class TranslationMetrics:
     Contains
     - Token Accuracy
     - 1 - Token Accuracy
-    - Mean F1
     - BLEU
     """
 
     token_acc: float = attr.ib(factory=float)
     token_err: float = attr.ib(default=1.0)
-    mean_f1: float = attr.ib(factory=float)
     bleu: float = attr.ib(factory=float)
     rounding: int = attr.ib(default=5)
     language: str = attr.ib(default="")
@@ -64,18 +59,15 @@ class TranslationMetrics:
     def __attrs_post_init__(self) -> None:
         self.token_acc = round(self.token_acc, self.rounding)
         self.token_err = round(self.token_err, self.rounding)
-        self.mean_f1 = round(self.mean_f1, self.rounding)
         self.bleu = round(self.bleu, self.rounding)
 
     def format(self) -> str:
         """Format like in old evaluate.py"""
 
         out = """Token Accuracy\t{token_acc:.4f}
-Mean F1\t{mean_f1:.4f}
 WER\t{token_err:.4f}
 BLEU\t{bleu:.4f}\n\n""".format(
             token_acc=self.token_acc,
-            mean_f1=self.mean_f1,
             token_err=self.token_err,
             bleu=self.bleu,
         )
@@ -102,12 +94,10 @@ class TranslationResults:
         token_acc = 100 * self.token_accuracy(self.system_outputs)
         token_err = 100 - token_acc
         bleu = 100 * self.bleu(self.system_outputs)
-        mean_f1 = 100 * self.mean_f1(self.system_outputs)
 
         metrics = TranslationMetrics(
             token_acc=token_acc,
             token_err=token_err,
-            mean_f1=mean_f1,
             bleu=bleu,
             language=language,
         )
@@ -123,25 +113,6 @@ class TranslationResults:
         bleu = sacrebleu.corpus_bleu(hypotheses, references, force=True)
 
         return bleu.score / 100.0  # divide to normalize
-
-    def mean_f1(self, system_outputs: List[TranslationOutput]) -> float:
-        return np.mean([self.f1(o.reference, o.hypothesis) for o in system_outputs])
-
-    def f1(self, src: str, tgt: str) -> float:
-        def lcs(src: str, tgt: str) -> float:
-            lcs = 0.5 * ((len(src) + len(tgt)) - editdistance.eval(src, tgt))
-
-            return lcs
-
-        try:
-            rec = lcs(src, tgt) / len(tgt)
-        except ZeroDivisionError:
-            rec = 0
-        prec = lcs(src, tgt) / len(src)
-        try:
-            return 2 * ((rec * prec) / (rec + prec))
-        except ZeroDivisionError:
-            return 0
 
 
 @attr.s(kw_only=True)
@@ -177,13 +148,21 @@ class ExperimentResults:
         source_path: str,
         languages_path: str,
     ) -> Tuple[List[TranslationOutput], Set[str]]:
+        def global_lang_generator():
+            while True:
+                yield "global"
+
         with read_text(hypotheses_path) as hyp, read_text(
             references_path
-        ) as ref, read_text(source_path) as src, read_text(languages_path) as langs:
+        ) as ref, read_text(source_path) as src, (
+            read_text(languages_path) if languages_path else global_lang_generator()
+        ) as langs:
             languages = set()
             system_outputs = []
 
-            for hyp_line, ref_line, src_line, langs_line in zip(hyp, ref, src, langs):
+            for hyp_line, ref_line, src_line, langs_line in it.zip_longest(
+                hyp, ref, src, langs
+            ):
 
                 # grab hypothesis lines
                 hypothesis = hyp_line.strip()
@@ -282,8 +261,8 @@ class ExperimentResults:
             .rename(
                 columns={
                     "token_acc": "Accuracy",
-                    "mean_f1": "F1",
                     "language": "Language",
+                    "bleu": "BLEU",
                 }
             )
             .round(3)
