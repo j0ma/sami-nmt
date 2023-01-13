dc0efaa1638add9ab126f348f9b0dcd633d343b8
Jonne Sälevä <jonne@jonnesaleva.com>
2023-01-12 21:02:06 -0500

    Estonian max update 100k
    Fix eng-est data folder

--- config/english2estonian_sweep_randbpe_cfg.sh
+++ config/english2estonian_sweep_randbpe_cfg.sh
@@ -4,7 +4,7 @@ export randseg_experiment_name=english2estonian
 export randseg_pick_randomly=yes
 export randseg_uniform=no
 export randseg_root_folder=./experiments
-export randseg_raw_data_folder=./data/est-eng
+export randseg_raw_data_folder=./data/eng-est/big
 export randseg_source_language=eng
 export randseg_target_language=est
 export randseg_checkpoints_folder=./bin/sweep_randseg_ckpt_eng_est_randbpe_${randseg_num_merges}mops_${randseg_random_seed}_temperature${randseg_temperature}_$(date +%s)
@@ -12,4 +12,6 @@ export randseg_binarized_data_folder=./bin/sweep_randseg_bindata_eng_est_randbpe
 export randseg_model_name=transformer_randbpe_${randseg_num_merges}mops_${randseg_random_seed}_temperature${randseg_temperature}
 
 mkdir -p $randseg_checkpoints_folder $randseg_binarized_data_folder
+
 export randseg_max_tokens="12000"
+export randseg_max_update="100000"
