2d817d259c9f8f3fcbd6a630a435497e4d0deaa0
Jonne Saleva <jonnesaleva@brandeis.edu>
2023-01-25 12:31:10 -0500

    Add BLEU and call to scripts/analyze_text_data.sh to scripts/analyze_experiment.sh

--- scripts/analyze_experiment.sh
+++ scripts/analyze_experiment.sh
@@ -10,6 +10,7 @@ sweep_cfg_folder=${2:-""}
 # constants
 train_folder="$experiment_path/train"
 eval_folder="$experiment_path/eval"
+supp_folder="$train_folder/supplemental_data"
 
 # how many started/finished?
 num_started=$(ls $train_folder | wc -l)
@@ -25,4 +26,17 @@ else
     echo "Num started: ${num_started} / ${n_total_configs}"
     echo "Num finished: ${num_finished} / ${n_total_configs}"
 fi
+
+# Sweep results (BLEU)
+echo "Getting sweep results..."
+parallel --tag --bar --progress \
+    "tail -n 1 {1} | cut -f2" ::: \
+    $eval_folder/*/valid.eval.score | \
+    sort | tee $experiment_path/sweep_results.tsv
+
+# Analyze the text data
+echo "Analyzing BPE text data..."
+parallel --jobs 1 --bar --progress \
+    "bash scripts/analyze_text_data.sh {1} eng uzb" ::: \
+    $(find $train_folder -type d -name supplemental_data)
 
