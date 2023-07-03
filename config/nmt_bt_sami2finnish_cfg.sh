840d3721fcadb1d9bae1dca11640e60c01519351
Jonne Sälevä <jonnesaleva@brandeis.edu>
2023-07-03 12:00:47 -0400

    fix BT configs

---
+++ config/nmt_bt_sami2finnish_cfg.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export randseg_pick_randomly=no
+export randseg_uniform=no
+export randseg_root_folder=./experiments
+export randseg_raw_data_folder=./data/fin-sme/nmt_bt
+export randseg_source_language=sme
+export randseg_target_language=fin
+export randseg_checkpoints_folder=./sme_fin_bin/sweep_randseg_ckpt_sme_fin_vanillabpe_nmt_bt_${randseg_num_merges}mops_${randseg_random_seed}_temperature${randseg_temperature}_$(date +%s)
+export randseg_binarized_data_folder=./sme_fin_bin/sweep_randseg_bindata_sme_fin_vanillabpe_nmt_bt_${randseg_num_merges}mops_${randseg_random_seed}_temperature${randseg_temperature}_$(date +%s)
+export randseg_model_name=transformer_vanillabpe_nmt_bt_${randseg_num_merges}mops_${randseg_random_seed}_temperature${randseg_temperature}
+
+mkdir -p $randseg_checkpoints_folder $randseg_binarized_data_folder
+
+export randseg_max_tokens="3600" #0"
+export randseg_max_update="10000"
