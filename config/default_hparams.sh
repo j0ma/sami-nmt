e75a35870909e719ac58e430ab82d01a4091ded9
Jonne Saleva <jonnesaleva@brandeis.edu>
2023-07-07 16:32:21 -0400

    Minor cleanup: remove commented out lines from default_hparams.sh

--- config/default_hparams.sh
+++ config/default_hparams.sh
@@ -32,8 +32,3 @@ export randseg_update_freq=16
 export randseg_uniform="no"
 export randseg_train_on_dev="no"
 export randseg_tie_all_embeddings="yes"
-
-#export randseg_should_create_experiment=yes
-#export randseg_should_preprocess=yes
-#export randseg_should_train=yes
-#export randseg_should_evaluate=yes
