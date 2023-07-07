b2c04fa08dfaf85d0ab6fad85e218c3720100d78
Jonne Saleva <jonnesaleva@brandeis.edu>
2023-07-07 16:43:03 -0400

    analyze_lengths.py: create table using pandas

--- scripts/analyze_lengths.py
+++ scripts/analyze_lengths.py
@@ -1,6 +1,6 @@
 import sys
 import statistics
-from tabulate import tabulate
+import pandas as pd
 
 # Parse command-line arguments to get list of input file names
 input_files = sys.argv[1:]
@@ -10,11 +10,13 @@ num_tokens = {input_file: [] for input_file in input_files}
 
 # Read lines from input files and compute the of space-separated tokens on each line
 for input_file in input_files:
-    with open(input_file, 'r') as f:
+    with open(input_file, "r") as f:
         for line in f:
-            line = line.strip() # remove trailing newline
-            tokens = line.split() # split line into tokens
-            num_tokens[input_file].append(len(tokens)) # add of tokens to list for input file
+            line = line.strip()  # remove trailing newline
+            tokens = line.split()  # split line into tokens
+            num_tokens[input_file].append(
+                len(tokens)
+            )  # add of tokens to list for input file
 
 # Compute mean, median, and standard deviation of for each input file
 results = []
@@ -33,8 +35,20 @@ for input_file in input_files:
 
 # Format the results as a table
 headers = ["Input file", "Statistic", "Value"]
-results.sort(key=lambda x: x[0]) # sort results by second column, then by first column
-table = tabulate(results, headers=headers)
+results.sort(key=lambda x: x[0])  # sort results by second column, then by first column
+
+results = (
+    pd.DataFrame(results, columns=["filename", "statistic", "value"])
+    .set_index(["filename", "statistic"])
+    .unstack()
+    .droplevel(axis=1, level=0)
+    .reset_index()
+)
+
+rename_dict = {old: new for old, new in zip(["filename", "statistic", "value"], headers)}
+results = results.rename(columns=rename_dict).set_index("Input file")
+
+table = results.to_markdown(index=True)
 
 # Print the table
 print(table)
