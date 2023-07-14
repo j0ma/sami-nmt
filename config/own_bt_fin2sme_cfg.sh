83b9a23a1db7cf1ad625eef9718a0a5944bcb199
Jonne Saleva <jonnesaleva@brandeis.edu>
2023-07-14 11:30:09 -0400

    configs for fin2sme with own backtranslation data

---
+++ config/own_bt_fin2sme_cfg.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export randseg_pick_randomly=no
+export randseg_uniform=no
+export randseg_root_folder=./experiments
+export randseg_raw_data_folder=./data/fin-sme/own_bt
+export randseg_source_language=fin
+export randseg_target_language=sme
+export randseg_checkpoints_folder=./fin_sme_bin/own_bt_checkpoints_$(date +%s)
+export randseg_binarized_data_folder=./fin_sme_bin/own_bt_bindata_$(date +%s)
+export randseg_model_name=own_bt
+
+mkdir -p $randseg_checkpoints_folder $randseg_binarized_data_folder
+
+export randseg_max_tokens="16000" 
+export randseg_max_update="15000"
