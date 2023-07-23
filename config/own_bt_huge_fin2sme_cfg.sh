0ad7cf9d22b97ffd2a52394344fcbf33e3dcd50f
Jonne Saleva <jonnesaleva@brandeis.edu>
2023-07-23 17:41:29 -0400

    Make own bt / own bt huge distinction explicit

---
+++ config/own_bt_huge_fin2sme_cfg.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export randseg_pick_randomly=no
+export randseg_uniform=no
+export randseg_root_folder=./experiments
+export randseg_raw_data_folder=./data/fin-sme/own_bt_huge
+export randseg_source_language=fin
+export randseg_target_language=sme
+export randseg_checkpoints_folder=./fin_sme_bin/own_bt_checkpoints_$(date +%s)
+export randseg_binarized_data_folder=./fin_sme_bin/own_bt_bindata_$(date +%s)
+export randseg_model_name=own_bt_huge
+
+mkdir -p $randseg_checkpoints_folder $randseg_binarized_data_folder
+
+export randseg_max_tokens="17000" 
+export randseg_max_update="15000"
--- sweep_experiment.sh
+++ sweep_experiment.sh
@@ -64,9 +64,13 @@ run_single_exp () {
             export randseg_cfg_file=./config/clean_nmt_all_rbmt_bt_${randseg_direction}_cfg.sh
             export randseg_raw_data_folder=./data/fin-sme/clean_nmt_all_rbmt_bt
             ;;
+        own_huge)
+            export randseg_cfg_file=./config/own_bt_huge_${randseg_direction}_cfg.sh
+            export randseg_raw_data_folder=./data/fin-sme/own_bt_huge
+            ;;
         own)
             export randseg_cfg_file=./config/own_bt_${randseg_direction}_cfg.sh
-            export randseg_raw_data_folder=./data/fin-sme/own_bt
+            export randseg_raw_data_folder=./data/fin-sme/own_bt_onceonly
             ;;
         *)
             exit
