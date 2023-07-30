9491f294648f8e3c0e9fe4a4f6905540a2459134
Jonne Saleva <jonnesaleva@brandeis.edu>
2023-07-30 16:00:31 -0400

    split wiki backtranslate configs -> filtered and unfiltered

--- config/backtranslate_sami_monolingual_data_wikipedia_filtered.sh
+++ config/backtranslate_sami_monolingual_data_wikipedia_filtered.sh
@@ -4,9 +4,9 @@ export randseg_target_language=fin
 export randseg_should_preprocess=yes
 export randseg_should_evaluate=yes
 export randseg_use_sentencepiece=yes
-export randseg_new_eval_name=bt_sme2fin_wikipedia
+export randseg_new_eval_name=bt_sme2fin_wikipedia_filtered
 
-export randseg_new_eval_raw_data_folder=${randseg_existing_train_folder}/raw_data/mono_to_bt_wikipedia
+export randseg_new_eval_raw_data_folder=${randseg_existing_train_folder}/raw_data/mono_to_bt_wikipedia_filtered
 export randseg_new_eval_binarized_data_folder=./fin_sme_bin/bt_fin2sme_${randseg_new_eval_name}_$(date +%s)
 mkdir -p $randseg_new_eval_binarized_data_folder
 
---
+++ config/backtranslate_sami_monolingual_data_wikipedia_unfiltered.sh
@@ -0,0 +1,13 @@
+export randseg_random_seed=1234
+export randseg_source_language=sme
+export randseg_target_language=fin
+export randseg_should_preprocess=yes
+export randseg_should_evaluate=yes
+export randseg_use_sentencepiece=yes
+export randseg_new_eval_name=bt_sme2fin_wikipedia_unfiltered
+
+export randseg_new_eval_raw_data_folder=${randseg_existing_train_folder}/raw_data/mono_to_bt_wikipedia_unfiltered
+export randseg_new_eval_binarized_data_folder=./fin_sme_bin/bt_fin2sme_${randseg_new_eval_name}_$(date +%s)
+mkdir -p $randseg_new_eval_binarized_data_folder
+
+export randseg_max_tokens=100000
