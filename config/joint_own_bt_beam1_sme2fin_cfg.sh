7b6e0f9bddf5ac8d8b981c1289a4f88fa58b19cb
Jonne Sälevä <jonne@jonnesaleva.com>
2023-09-24 20:57:23 -0400

    add joint scripts for beam1 too

---
+++ config/joint_own_bt_beam1_sme2fin_cfg.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+export randseg_pick_randomly=no
+export randseg_uniform=no
+export randseg_root_folder=./experiments
+export randseg_raw_data_folder=./data/fin-sme/own_bt_beam1
+export randseg_source_language=sme
+export randseg_target_language=fin
+export randseg_checkpoints_folder=./sme_fin_bin/own_bt_beam1_checkpoints_$(date +%s)
+export randseg_binarized_data_folder=./sme_fin_bin/own_bt_beam1_bindata_$(date +%s)
+export randseg_model_name=own_bt_beam1
+
+mkdir -p $randseg_checkpoints_folder $randseg_binarized_data_folder
+
+export randseg_max_tokens="17000" 
+export randseg_max_update="15000"
+export randseg_joint_subwords=yes
