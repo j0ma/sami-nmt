ecd6dc3b543113f45d474446e14eae1ea1f9466f
Jonne Sälevä <jonne@jonnesaleva.com>
2023-09-25 00:16:41 -0400

    char configs

---
+++ config/char_own_bt_huge_fin2sme_cfg.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+export randseg_pick_randomly=no
+export randseg_uniform=no
+export randseg_root_folder=./experiments
+export randseg_raw_data_folder=./data/fin-sme/own_bt_huge
+export randseg_source_language=fin
+export randseg_target_language=sme
+export randseg_checkpoints_folder=./fin_sme_bin/own_bt_huge_checkpoints_$(date +%s)
+export randseg_binarized_data_folder=./fin_sme_bin/own_bt_huge_bindata_$(date +%s)
+export randseg_model_name=own_bt_huge_${randseg_num_merges}merges
+
+mkdir -p $randseg_checkpoints_folder $randseg_binarized_data_folder
+
+export randseg_max_tokens="17000" 
+export randseg_max_update="15000"
+export character_level_model=yes
