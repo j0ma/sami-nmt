04b568c4ab1df9c91636c45ed7096c94bd8bdfc0
Jonne Saleva <jonnesaleva@brandeis.edu>
2023-09-29 17:14:45 -0400

    change batch size to max 20k tokens per GPU for joint experiments

--- config/joint_own_bt_sme2fin_cfg.sh
+++ config/joint_own_bt_sme2fin_cfg.sh
@@ -12,7 +12,7 @@ export randseg_model_name=own_bt_${randseg_num_merges}merges
 
 mkdir -p $randseg_checkpoints_folder $randseg_binarized_data_folder
 
-export randseg_max_tokens="17000" 
+export randseg_max_tokens="20000" 
 export randseg_max_update="15000"
 export randseg_joint_subwords=yes
 export randseg_lr=0.001
