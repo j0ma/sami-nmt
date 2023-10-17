0ccedb987e404616c201f19eb0f68923b5743986
Jonne Saleva <jonnesaleva@brandeis.edu>
2023-10-16 22:11:28 -0400

    add embdims to some joint configs

--- config/fin2sme/joint_own_bt_huge_fin2sme_cfg.sh
+++ config/fin2sme/joint_own_bt_huge_fin2sme_cfg.sh
@@ -7,7 +7,7 @@ export randseg_source_language=fin
 export randseg_target_language=sme
 export randseg_checkpoints_folder=./fin_sme_bin/own_bt_best_checkpoints_$(date +%s)
 export randseg_binarized_data_folder=./fin_sme_bin/own_bt_best_bindata_$(date +%s)
-export randseg_model_name=${randseg_train_data_type}_${randseg_encoder_hidden_size}hidden_${randseg_encoder_attention_heads}heads_${randseg_num_merges}merges_${randseg_p_dropout}dropout
+export randseg_model_name=${randseg_train_data_type}_${randseg_encoder_hidden_size}hidden_${randseg_encoder_attention_heads}heads_${randseg_num_merges}merges_${randseg_p_dropout}dropout_${randseg_encoder_embedding_dim}encembsize_${randseg_decoder_embedding_dim}decembsize
 
 mkdir -p $randseg_checkpoints_folder $randseg_binarized_data_folder
 
