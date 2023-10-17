a2036c3b7d023a649d8c1f92a390bb4774210ce3
Jonne Saleva <jonnesaleva@brandeis.edu>
2023-10-16 22:05:59 -0400

    eng2iku sync

--- config/eng2iku/joint_baseline_eng2iku_cfg.sh
+++ config/eng2iku/joint_baseline_eng2iku_cfg.sh
@@ -8,7 +8,7 @@ export randseg_source_language=eng
 export randseg_target_language=iku
 export randseg_checkpoints_folder=./eng_iku_bin/eng_iku_${randseg_train_data_type}_checkpoints_$(date +%s)
 export randseg_binarized_data_folder=./eng_iku_bin/eng_iku_${randseg_train_data_type}_binarized_data_$(date +%s)
-export randseg_model_name=${randseg_train_data_type}_${randseg_encoder_hidden_size}hidden_${randseg_encoder_attention_heads}heads_${randseg_num_merges}merges_${randseg_p_dropout}dropout
+export randseg_model_name=${randseg_train_data_type}_${randseg_encoder_hidden_size}hidden_${randseg_encoder_attention_heads}heads_${randseg_num_merges}merges_${randseg_p_dropout}dropout_${randseg_encoder_embedding_dim}encoderemb_${randseg_decoder_embedding_dim}decoderemb
 
 mkdir -p $randseg_checkpoints_folder $randseg_binarized_data_folder
 
